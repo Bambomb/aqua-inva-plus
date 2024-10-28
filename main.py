bc0b70abd83e16e66d67300e7c465d2ef31600f5
hitsu <202379963@cegepsherbrooke.qc.ca>
2024-10-28 15:37:21 -0400

    Fenêtre blanche

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #Le fichier main
+
+import customtkinter as ctk
+import tkinter as tk
+
+class MainApp(ctk.CTk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}+{-10}+{0}")
+        self.state("zoomed")
+        self.title("Application qu'on sait pas le nom")
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.mainloop()
