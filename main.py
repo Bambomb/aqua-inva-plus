07406faae9229bc01b70d8512cde0a27a6927545
max <202379963@cegepsherbrooke.qc.ca>
2024-10-30 09:13:32 -0400

    Continué le filtre de caractères mais marche toujours pas

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ class MainApp(ctk.CTk):
         super().__init__()
 
         self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}+{-10}+{0}")
-        self.state("zoomed")
         self.title("Application pêche invasive")
         
         self.searchWidget = SearchWidget(master=self)
--- recherche.py
+++ recherche.py
@@ -1,6 +1,7 @@
 #Widget de recherche et ses fonctionnalités
 
 import customtkinter as ctk
+import re
 
 class SearchWidget(ctk.CTkFrame):
     def __init__(self, master=None):
@@ -8,14 +9,20 @@ class SearchWidget(ctk.CTkFrame):
         self.master = master
         self.create_widgets()
 
-    def changed(self, text):
-        print("Changé")
+    def changed(self, event):
+        
+        text = self.champ.get() 
+        text+= event.char
+        filt_text = re.sub("[^a-zA-Z0-9à-üÀ-Ü .,-_]","",text)
+        self.champ.delete(0,2000000)
+        self.champ.insert(0,filt_text[0:-1])
+        print(filt_text)
 
     def create_widgets(self):
         self.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
         self.champ = ctk.CTkEntry(self, placeholder_text="Rechercher")
         self.champ.grid(row=0, column=0, pady=(10,10))
-        self.champ.bind('<<Modified>>', self.changed)
+        self.champ.bind("<Key>", self.changed)
 
 
 
