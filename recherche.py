8e4e20000f7858d3bd93a6d1bf4650d2d93d2943
max <202379963@cegepsherbrooke.qc.ca>
2024-10-30 10:22:11 -0400

    finalement je vais utiliser enter parce que c'est troip chiant

--- recherche.py
+++ recherche.py
@@ -9,20 +9,14 @@ class SearchWidget(ctk.CTkFrame):
         self.master = master
         self.create_widgets()
 
-    def changed(self, event):
-        
-        text = self.champ.get() 
-        text+= event.char
-        filt_text = re.sub("[^a-zA-Z0-9à-üÀ-Ü .,-_]","",text)
-        self.champ.delete(0,2000000)
-        self.champ.insert(0,filt_text[0:-1])
-        print(filt_text)
+    def enter(self, event):
+        text = re.sub("[^a-zA-Z0-9à-üÀ-Ü., _'-]","",self.champ.get())
+        while():
+            pass
+    
 
     def create_widgets(self):
         self.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
         self.champ = ctk.CTkEntry(self, placeholder_text="Rechercher")
         self.champ.grid(row=0, column=0, pady=(10,10))
-        self.champ.bind("<Key>", self.changed)
-
-
-
+        self.champ.bind("<Return>", self.enter)
