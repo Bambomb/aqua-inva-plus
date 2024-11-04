d8a5aeb0516513eb849dd1cc1d4ba516aa5de53f
max <202379963@cegepsherbrooke.qc.ca>
2024-11-04 17:28:44 -0500

    la recherche avec le fichier mais en fait ça marche pas

--- recherche.py
+++ recherche.py
@@ -11,15 +11,20 @@ class SearchWidget(ctk.CTkFrame):
         self.content.trace("w", lambda name, index,mode, var=self.content: self.changed(var))
         self.data = data
         self.label_collection = []
+        self.configure(height=self.master.winfo_screenheight()-200)
         self.create_widgets()
 
+#faire que ça marche et enlever certaines colonnes pour la recherche
     def search(self, text):
         results=[]
-        for line in self.data:
-            if text.upper() in line.upper():
-                results.append(line)
+        for column in self.data:
+            print(self.data[column])
+            for line in self.data[column]:
+                print("line",line)
+                if text.upper() in line.upper():results.append(line)
         self.display(results)
 
+#Ajouter un délai??
     def changed(self, event):
         text = event.get()
         if text!="" and text!=" ": self.search(text)
@@ -28,7 +33,7 @@ class SearchWidget(ctk.CTkFrame):
             self.frame()
 
     def create_widgets(self):
-        self.grid(row=0, column=0, padx=20, pady=20)
+        self.grid(row=0, column=0, padx=20, pady=20, sticky="n")
         self.pack_propagate(0)
 
         self.champ = ctk.CTkEntry(self, placeholder_text="Rechercher", textvariable=self.content)
@@ -43,12 +48,12 @@ class SearchWidget(ctk.CTkFrame):
 
         for i, result in enumerate(results):
             self.label_collection.append(ctk.CTkLabel(self.resultats, text=result, fg_color="white", width=197))
-            self.label_collection[i].pack(expand=True,)
+            self.label_collection[i].pack(expand=True,side=tk.TOP)
 
     def frame(self):
-        self.resultats = ctk.CTkFrame(self, width=200, height=500, border_width=2, border_color="black")
+        self.resultats = ctk.CTkFrame(self, width=200, height=1000, border_width=2, border_color="black")
         self.resultats.pack_propagate(0)
-        self.resultats.pack()
+        self.resultats.pack(expand=1,fill="both")
 
    
 
