4b623174eb1590968c9db13a4a3ed804100b571c
max <202379963@cegepsherbrooke.qc.ca>
2024-11-08 11:26:30 -0500

    On peut cliquer sur les labels par contre ça fait rien

--- recherche.py
+++ recherche.py
@@ -6,6 +6,7 @@ class SearchWidget(ctk.CTkFrame):
     def __init__(self, data, master=None):
         super().__init__(master)
         self.master = master
+
         self.content = tk.StringVar()
         self.content.trace("w", lambda name, index,mode, var=self.content: self.changed(var))
         self.data = data
@@ -31,15 +32,14 @@ class SearchWidget(ctk.CTkFrame):
 
         if(side>=0): #Si la recherche se fait en avant
             i = 0
-            for column in self.datasearch[self.x:]:
+            for column in self.datasearch[self.x:]: #Parcoure chaque colonne du dataframe
                 j = 0
-                for case in self.datasearch[column][self.y:]:
-                    case = str(case)
+                for case in self.datasearch[column][self.y:]: #Parcoure chaque case de la colonne
+                    case = str(case) #Transforme en string pour le upper()
                     j+=1
-                    if text.upper() in case.upper():
+                    if text.upper() in case.upper(): #Si la recherche se trouve dans la case
                         results.append(case)
-                        print("x,y : ", self.x, ", ", self.y)
-                        if(len(results)>=20):
+                        if(len(results)>=20): #Limite à 20 résultats
                             self.x+=i
                             self.y+=j
                             self.display(results)
@@ -53,7 +53,7 @@ class SearchWidget(ctk.CTkFrame):
     #Fonction event callback du widget entry qui réagit quand le texte change
     def changed(self, event):
         self.text = event.get()
-        if self.text!="" and self.text!=" ": self.search(self.text, 0)
+        if self.text!="" and self.text!=" ": self.search(self.text, 0) #S'assure qu'il y a une entrée dans le champ
         else:
             self.resultats.destroy()
             self.frame()
@@ -63,32 +63,41 @@ class SearchWidget(ctk.CTkFrame):
         self.grid(row=0, column=0, padx=20, pady=20, sticky="n")
         self.pack_propagate(0)
 
+        #Entry du champ de recherche
         self.champ = ctk.CTkEntry(self, placeholder_text="Rechercher", textvariable=self.content)
         self.champ.pack()
 
+        #Crée le frame de résultats
         self.frame()
 
+        #Frame du menu pour changer de page
         self.pagemenu = ctk.CTkFrame(self)
         self.pagemenu.pack(side=tk.BOTTOM)
 
+        #Bouton pour retourner au début
         self.buttonleft = ctk.CTkButton(self.pagemenu, text="|<-", command=self.gauche, width=30)
         self.buttonleft.grid(row=0, column=0, sticky="n", padx=20)
 
+        #Bouton pour aller à la page suivante
         self.buttonright = ctk.CTkButton(self.pagemenu, text="->", command=self.droite, width=30)
         self.buttonright.grid(row=0, column=2, sticky="n", padx=20)
 
+        #Label qui affiche le numéro de page
         self.labelpage = ctk.CTkLabel(self.pagemenu, text = 1)
         self.labelpage.grid(row=0, column=1, sticky="n", padx=5)
 
     #Fonction d'affichage des résultats
     def display(self, results):
+        #Rafraîchissement
         self.resultats.destroy()
         self.labelpage.configure(text=self.nb_page)
         self.label_collection=[]
         self.frame()
 
+        #Crée un widget label pour chacun des résultats
         for i, result in enumerate(results):
             self.label_collection.append(ctk.CTkLabel(self.resultats, text=result, fg_color="white", width=197))
+            self.label_collection[i].bind("<ButtonRelease-1>", lambda e, command=i:self.on_res_click(), add=True)
             self.label_collection[i].pack(expand=True,side=tk.TOP)
 
     #Fonction qui crée le frame des résultats
@@ -109,3 +118,6 @@ class SearchWidget(ctk.CTkFrame):
         if(self.max==False):
             self.nb_page +=1
             self.search(self.text, 1)
+
+    def on_res_click(self):
+        print("Cliqué")
